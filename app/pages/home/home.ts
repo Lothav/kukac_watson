1aa16835660e32997c3c52f96c9ed20f5c2cf894
Luiz0tavio <luizotrv@gmail.com>
2016-09-10 11:46:20 -0300

    camera getPic()

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,27 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, Page} from 'ionic-angular';
+import {Camera} from 'ionic-native';
 
 @Component({
-  templateUrl: 'build/pages/home/home.html'
+    templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
-  
-  }
+    public base64Image: string;
+
+    constructor(private navCtrl: NavController) {
+
+    }
+    public takePicture(){
+        Camera.getPicture({
+            destinationType: 0,
+            targetWidth: 1000,
+            targetHeight: 1000
+        }).then((imageData) => {
+            // imageData is a base64 encoded string
+            this.base64Image = "data:image/jpeg;base64," + imageData;
+        }, (err) => {
+            this.base64Image = err;
+        });
+    }
+
 }
